a037da67fec454a8af0157f2ab5745dbf320bcf0
bilen2020 <64380790+bilen2020@users.noreply.github.com>
2022-09-01 15:55:18 -0400

    Fix typo

--- server.js
+++ server.js
@@ -39,11 +39,11 @@ fs.readFile(filename, 'utf8',(err, data) => {
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-const server = http.createServer(req, res) => {
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
     res.end(fs.readFile)
-}
+})
 
 
 
